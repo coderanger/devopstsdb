ee910959ceb49cb6d80f8baaced666eb832b8914
Noah Kantrowitz <noah@coderanger.net>
2011-06-18 18:18:08 -0700

    Finalize deploy recipe.

--- chef/cookbooks/devopstsdb/recipes/default.rb
+++ chef/cookbooks/devopstsdb/recipes/default.rb
@@ -5,6 +5,17 @@ directory "/srv/devopstsdb" do
   group "www-data"
 end
 
+python_virtualenv "/srv/devopstsdb/.venv" do
+  action :create
+  owner "www-data"
+  group "www-data"
+end
+
+execute "devopstsdb requirements" do
+  execute :nothing
+  command "/srv/devopstsdb/.venv/bin/pip install -r /srv/devopstsdb/current/app/requirements.txt"
+end
+
 deploy_revision "/srv/devopstsdb" do
   repository "git://github.com/coderanger/devopstsdb.git"
   user "www-data"
@@ -13,11 +24,7 @@ deploy_revision "/srv/devopstsdb" do
   create_dirs_before_symlink []
   symlinks({})
   symlink_before_migrate({})
+  notify :execute, 'execute[devopstsdb requirements]'
 end
 
-python_virtualenv "/srv/devopstsdb/.venv" do
-  action :create
-  owner "www-data"
-  group "www-data"
-end
 
