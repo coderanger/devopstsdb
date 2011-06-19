1ce9dbbdf6529b3661e7a42eebb0e5b24eedfa34
Duncan Phillips <duncan.h.phillips@gmail.com>
2011-06-18 18:53:01 -0700

    working get/set

--- app/apiservice.py
+++ app/apiservice.py
@@ -11,11 +11,15 @@ class dal:
     def __init__(self, host=None, port=6379, db=0):
         self.connection = redis.Redis(host=host, port=port, db=db)
 
-    def setkv(self,key,val):
-        return self.connection.set(key,val)
+    def hset(self,key,hkey,hval):
+        return self.connection.set("%s-%s"%(key,hkey),hval)
 
     def getkey(self,key):
         return self.connection.get(key)
+
+    def keys(self, pattern='*'):
+        print self.connection.keys()[0]
+        return self.connection.keys(pattern)
 
 
 db=dal(host='184.72.185.234')
@@ -33,18 +37,40 @@ def retrieve():
     try:
         start = datetime.strptime(start,'%Y-%m-%dT%H:%M:%S')
     except:
+        import sys
+        print sys.exc_info()
         start = datetime.now()-datetime.timedelta(seconds=300)
 
     try:
         end = datetime.strptime(end,'%Y-%m-%dT%H:%M:%S')
     except:
+        import sys
+        print sys.exc_info()
         end = datetime.now()
 
-    if key == None: return "No key specified"
+    all_rkeys = db.keys().split()
+    data = []
 
-    print "Args: start: %s, end: %s, key: %s" % (str(start), str(end), str(key))
+    print "keys:%s" % all_rkeys
 
-    return "\n\nDone\n\n"
+    for rkey in all_rkeys:
+        try:
+            sdate,k=rkey.rsplit('-',1)
+            print 'splitting key into date-metric: %s - %s' % (sdate,k)
+            kdate = datetime.strptime(sdate,'%Y-%m-%dT%H:%M:%S')
+        except Exception, e:
+            print "Date in wrong format: %s\n" % rkey
+            print e
+            pass
+
+        if (kdate >= start) and (kdate <= end) and (k == key):
+            v = db.getkey(rkey)
+            data.append({'date':sdate, 'key':k, 'value':v})
+        else:
+            pass
+
+
+    return "\n\n%s\n\n" % data
 
 
 @app.route("/set/",methods=["POST"])
@@ -55,7 +81,7 @@ def setvalue():
     results=[]
     try:
         for kv in request.json:
-            result = db.setkv(kv['ts'],{kv['key']:kv['value']})
+            result = db.hset(kv['ts'],kv['key'],kv['value'])
             results.append(result)
     except:
         import sys
